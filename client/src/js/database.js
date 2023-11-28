9c40bb68d5a54d6ee840448c58018e1c0e506221
Janet Webster <twixmixy@gmail.com>
2023-11-28 10:16:23 -0500

    Update database name in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,14 +1,14 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('text-editor', 1, {
+  openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('text-editor')) {
-        console.log('text-editor database already exists');
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('text-editor', { keyPath: 'id', autoIncrement: true });
-      console.log('text-editor database created');
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
     },
   });
 
@@ -18,13 +18,13 @@ export const putDb = async (content) => {
     console.log("Updated the database");
 
     // Create connection to DB with version to use
-    const teDB = await openDB("text-editor", 1);
+    const jateDB = await openDB("jate", 1);
 
     // Create transaction with specific DB and priviledges
-    const text = teDB.transaction("text-editor", "readwrite");
+    const tx = jateDB.transaction("jate", "readwrite");
 
     // Open the object store
-    const store = text.objectStore("text-editor");
+    const store = tx.objectStore("jate");
 
     // Use put to update the database // one field to repeat retrieve and update
     const request = store.put({ id: 1, value: content });
@@ -43,13 +43,13 @@ export const getDb = async () => {
     console.log("Retrieve database");
 
     // Create connection to DB with version to use
-    const teDB = await openDB("text-editor", 1);
+    const jateDB = await openDB("jate", 1);
 
     // Create transaction with specific DB and priviledges
-    const text = teDB.transaction("text-editor", "readwrite");
+    const tx = jateDB.transaction("jate", "readwrite");
 
     // Open the object store
-    const store = text.objectStore("text-editor");
+    const store = tx.objectStore("jate");
 
     // Use get to retrieve one entry from DB
     const request = store.get(1);
